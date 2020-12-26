014a9d3e75feeb10b03e83bb5d85c90bddab28e8
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-12-26 09:11:41 +0530

    techapti
    alphabets

---
+++ pattern/designPatterns/Pattern7.java
@@ -0,0 +1,31 @@
+package com.xworkz.pattern.designPatterns;
+
+public class Pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=0;i<=4;i++) {
+			
+			
+			
+			for(int j=0;j<=4;j++) {
+				if(j>=i) {
+			
+				System.out.print("*");
+				}
+	
+				else {
+					System.out.print(" ");
+					
+				}
+			}
+			System.out.println( );
+
+}
+	}
+
+
+
+	}
+
+
